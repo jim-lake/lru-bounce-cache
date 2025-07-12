8f66bd071a70ca212edc3b76d78593675cbb55d5
jlake <jim@blueskylabs.com>
2025-07-12 11:16:59 -0700

    more docs

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ lru-bounce-cache
 ================
 
 LRU Cache with O(1) access
+
+## Usage
+
+```javascript
+const LRUBounceCache = require('lru-bounce-cache');
+const cache = new LRUBounceCache({ max_items: 1000, buffer_count: 2 });
+
+cache.set('f', 1000);
+cache.get('f');
+```
+
+## Description
+
+This library will maintain `buffer_count` maps of the `max_items` maximum
+length.
+
+A set will always write to the most recent buffer.  If that buffer is full,
+it will also allocate a new empty buffer as the most recent on buffer.
+This will evict the oldest buffer.
+
+Gets search all buffers to find the item.  If an item is found in not the most
+recent buffer, it writes to the most recent buffer.  If a get causes a write
+it might also cause an exict according to the set behavior.
+
+The maximum number of cached items is `max_items * buffer_count - 1`.
--- test/simple.test.ts
+++ test/simple.test.ts
@@ -1,6 +1,5 @@
-
 import { expect } from 'chai';
-import { LRUBounceCache } from '../src/index';
+import { LRUBounceCache } from '../src/lru-bounce-cache';
 
 describe('LRUBounceCache', () => {
   it('should be able to set and get a value', () => {
@@ -11,7 +10,7 @@ describe('LRUBounceCache', () => {
 
   it('should return undefined for a key that does not exist', () => {
     const cache = new LRUBounceCache<number>();
-    expect(cache.get('non-existent-key')).to.be.undefined;
+    expect(cache.get('non-existent-key')).to.equal(undefined);
   });
 
   it('should have a hit rate of 1.0 after one hit', () => {
@@ -35,33 +34,46 @@ describe('LRUBounceCache', () => {
     expect(cache.hit_rate()).to.equal(0.5);
   });
 
-  it('should evict the least recently used item when the cache is full', () => {
+  it('get bounce', () => {
     const cache = new LRUBounceCache<number>({ max_items: 3 });
 
     cache.set('a', 1);
     cache.set('b', 2);
     cache.set('c', 3);
-
-    // At this point, the primary cache is full.
-    // The next set should cause a bounce.
-
     cache.set('d', 4);
-
-    // The primary cache should now contain only 'd'.
-    // The secondary cache should contain 'a', 'b', and 'c'.
-
-    expect(cache.get('a')).to.equal(1);
-    expect(cache.get('b')).to.equal(2);
-    expect(cache.get('c')).to.equal(3);
-    expect(cache.get('d')).to.equal(4);
-
-    // Now, let's access 'a' to move it to the primary cache.
-    cache.get('a');
-
-    // Let's add another item to trigger another bounce.
     cache.set('e', 5);
 
-    // Now, 'b' should be evicted.
-    expect(cache.get('b')).to.be.undefined;
+    // check the top ones, this wont cause a write
+    expect(cache.get('e')).to.equal(5);
+    expect(cache.get('d')).to.equal(4);
+
+    // check a get bounce
+    expect(cache.get('c')).to.equal(3);
+
+    // c bounced these out
+    expect(cache.get('b')).to.equal(undefined);
+    expect(cache.get('a')).to.equal(undefined);
+  });
+
+  it('set bounce', () => {
+    const cache = new LRUBounceCache<number>({ max_items: 3 });
+
+    cache.set('a', 1);
+    cache.set('b', 2);
+    cache.set('c', 3);
+    cache.set('d', 4);
+    cache.set('e', 5);
+
+    // check the top ones, this wont cause a write
+    expect(cache.get('e')).to.equal(5);
+    expect(cache.get('d')).to.equal(4);
+
+    // check a set bounce
+    cache.set('f', 6);
+
+    // f bounced these out
+    expect(cache.get('c')).to.equal(undefined);
+    expect(cache.get('b')).to.equal(undefined);
+    expect(cache.get('a')).to.equal(undefined);
   });
 });
